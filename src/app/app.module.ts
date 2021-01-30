4640c87f2a7f27112ce0e2e741518878f09b7a24
Gabriel Paredes <gabriel@axented.com>
2021-01-30 13:53:50 -0500

    Remove extra font awesome (angular) dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgSelect2Module } from 'ng-select2';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
@@ -20,7 +19,6 @@ import { Daterangepicker } from 'ng2-daterangepicker';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule,
     BrowserAnimationsModule,
     FormsModule,
     NgbModule,
