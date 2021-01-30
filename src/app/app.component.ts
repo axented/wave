89c7218fd6930dfa05b763143d29be13e5b04dba
Gabriel Paredes <gabriel@axented.com>
2021-01-30 18:34:37 -0500

    Fix Feather Icon imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+// TODO remove below. Not sure why @types/feather not working
+// @ts-ignore
+import * as Feather from 'feather-icons/dist/feather.min.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mexiwave';
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit() {
+  	Feather.replace()
+  }
 }
