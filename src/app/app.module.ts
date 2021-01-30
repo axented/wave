445b10999e92eab3619929a79926977ab6d725b1
Gabriel Paredes <gabriel@axented.com>
2021-01-30 12:51:16 -0500

    Add forms, bootstrap, datepicker and sleect2 modules to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgSelect2Module } from 'ng-select2';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { Daterangepicker } from 'ng2-daterangepicker';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    NgbModule,
+    Daterangepicker,
+    NgSelect2Module,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
