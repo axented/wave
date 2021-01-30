d3eae499d2c54c41a450fb0b83f0fd2558905780
Gabriel Paredes <gabriel@axented.com>
2021-01-30 13:12:40 -0500

    Add routes for login, register and forgot-password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login-form', pathMatch: 'full' },  
+  {
+    path: 'login-form',
+    loadChildren: () =>
+      import('./unauth/login/login.module')
+        .then((m) => m.LoginModule
+      ),
+  },
+  {
+    path: 'forgot-pass',
+    loadChildren: () =>
+      import('./unauth/forgot-password/forgot-password.module')
+        .then((m) => m.ForgotPasswordModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () =>
+      import('./unauth/regiser/register.module')
+        .then((m) => m.RegisterModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      onSameUrlNavigation: 'reload',
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
