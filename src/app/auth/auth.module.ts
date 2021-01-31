9e88db6cb0ba5e5daeac9bb0acd3f3d20b69cbe0
Gabriel Paredes <gabriel@axented.com>
2021-01-31 16:54:38 -0500

    Add ng bootstrap collapse in auth component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [AuthComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    NgbModule
   ],
   bootstrap: [AuthComponent]
 })
