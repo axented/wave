de7dc029c851260c7b8ee8d9a3d32f351e62f29b
Gabriel Paredes <gabriel@axented.com>
2021-01-31 00:13:17 -0500

    Add Firebase Login to component and FireAuthGuard to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login-form']);
+const redirectLoggedInToItems = () => redirectLoggedInTo(['auth']);
 
 const routes: Routes = [
   { path: '', redirectTo: 'login-form', pathMatch: 'full' },  
   {
     path: 'login-form',
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectLoggedInToItems },
     loadChildren: () =>
       import('./unauth/login/login.module')
         .then((m) => m.LoginModule
@@ -12,18 +18,24 @@ const routes: Routes = [
   },
   {
     path: 'forgot-pass',
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectLoggedInToItems },
     loadChildren: () =>
       import('./unauth/forgot-password/forgot-password.module')
         .then((m) => m.ForgotPasswordModule)
   },
   {
     path: 'register',
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectLoggedInToItems },
     loadChildren: () =>
       import('./unauth/register/register.module')
         .then((m) => m.RegisterModule)
   },
   {
     path: 'auth',
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
     loadChildren: () =>
       import('./auth/auth.module')
         .then((m) => m.AuthModule)
