bac0c17048a567eb827700979d2b9febcf94fcde
Gabriel Paredes <gabriel@axented.com>
2021-01-31 22:45:05 -0500

    Add register with email to html and ts

--- src/app/unauth/register/register.component.ts
+++ src/app/unauth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+  confirmPassword: string;
+
+ constructor(
+    public auth: AngularFireAuth,
+    public router: Router
+    ) {
+    this.email = '';
+    this.password = '';
+    this.confirmPassword = '';
+ }
 
   ngOnInit(): void {
   }
+
+  googleLogin() {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    .then((result) => {
+      this.router.navigate(['auth'])
+    }).catch((error) => {
+      console.error(error.code)
+      console.error(error.message)
+    })
+  }
+
+  facebookLogin() {
+    alert('Coming Soon!')
+  }
+
+  registerUser() {
+    this.auth.createUserWithEmailAndPassword(this.email, this.password)
+      .then((userCredential) => {
+        this.router.navigate(['auth'])
+      })
+      .catch((error) => {
+        console.error(error.code)
+        console.error(error.message)
+
+      })
+  }
+
 
 }
