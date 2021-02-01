572d13548c818528f6f0f48dae4c8166080bafa4
Gabriel Paredes <gabriel@axented.com>
2021-01-31 23:04:00 -0500

    Add login with email and password in login component

--- src/app/unauth/login/login.component.ts
+++ src/app/unauth/login/login.component.ts
@@ -10,10 +10,16 @@ import firebase from 'firebase/app';
 })
 export class LoginComponent implements OnInit {
 
+  email: string;
+  password: string;
+
   constructor(
     public auth: AngularFireAuth,
     public router: Router
-    ) { }
+    ) {
+    this.email = '';
+    this.password = '';
+  }
 
   ngOnInit(): void {
   }
@@ -29,6 +35,17 @@ export class LoginComponent implements OnInit {
 
   facebookLogin() {
   	alert('Coming Soon!')
+  }
+
+  emailLogin() {
+    this.auth.signInWithEmailAndPassword(this.email, this.password)
+      .then((userCredential) => {
+        this.router.navigate(['auth'])
+      })
+      .catch((error) => {
+        console.error(error.code)
+        console.error(error.message)
+      })
   }
 
 }
