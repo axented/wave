bad8e0192c686c1df600ac8bf552b92c7fca5480
Gabriel Paredes <gabriel@axented.com>
2021-01-31 19:11:22 -0500

    Add HttpClientModule to app module with mock data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { Daterangepicker } from 'ng2-daterangepicker';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { DataService } from './auth/data.service';
+
 
 @NgModule({
   declarations: [
@@ -26,6 +30,8 @@ import { Daterangepicker } from 'ng2-daterangepicker';
     NgbModule,
     Daterangepicker,
     NgSelect2Module,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(DataService),
     AngularFireModule.initializeApp(environment.firebase)
     
   ],
